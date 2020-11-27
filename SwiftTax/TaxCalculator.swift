0b041f7017734d7b8a4291aceff87cf4b13154f7
Louis Kirkham <chew@talk21.com>
2020-11-27 19:46:31 +0000

    test possible edge case thresholds

--- SwiftTax/TaxCalculator.swift
+++ SwiftTax/TaxCalculator.swift
@@ -11,43 +11,43 @@ struct TaxCalculator {
     let taxThresholds = ["allowance": 12500.00, "basic": 50000.00, "allowanceReduction": 100000.00, "additional": 150000.00]
     let taxRates = ["basic": 0.20, "higher": 0.40, "additional": 0.45]
     
-    func deductTax(salary: Double) -> Double {
-        if salary > taxThresholds["additional"]! {
-            return salary - additionalRateTaxDeduction(salary: salary)
-        } else if salary > taxThresholds["allowanceReduction"]! {
-            return salary - higherRateWithAllowanceDeduction(salary: salary)
-        } else if salary > taxThresholds["basic"]! {
-            return salary - higherRateTaxDeduction(salary: salary)
-        } else if salary > taxThresholds["allowance"]! {
-            return salary - basicRateTaxDeduction(salary: salary)
+    func deductTax(gross: Double) -> Double {
+        if gross > taxThresholds["additional"]! {
+            return gross - additionalRateTaxDeduction(gross: gross)
+        } else if gross > taxThresholds["allowanceReduction"]! {
+            return gross - higherRateWithAllowanceDeduction(gross: gross)
+        } else if gross > taxThresholds["basic"]! {
+            return gross - higherRateTaxDeduction(gross: gross)
+        } else if gross > taxThresholds["allowance"]! {
+            return gross - basicRateTaxDeduction(gross: gross)
         } else {
-          return salary
+          return gross
         }
     }
     
     private
     
-    func additionalRateTaxDeduction(salary: Double) -> Double {
-        let additionalRateTaxable = salary - taxThresholds["additional"]!,
+    func additionalRateTaxDeduction(gross: Double) -> Double {
+        let additionalRateTaxable = gross - taxThresholds["additional"]!,
             additionalRateTax = additionalRateTaxable * taxRates["additional"]!
         return rounding(tax: additionalRateTax + basicBandTax()) + higherBandTax()
     }
     
-    func higherRateWithAllowanceDeduction(salary: Double) -> Double {
-        let reducedAllowance = reducePersonalAllowance(salary: salary),
-            higherRateTaxable = salary - taxThresholds["basic"]! + reducedAllowance,
+    func higherRateWithAllowanceDeduction(gross: Double) -> Double {
+        let reducedAllowance = reducePersonalAllowance(gross: gross),
+            higherRateTaxable = gross - taxThresholds["basic"]! + reducedAllowance,
             higherRateTax = higherRateTaxable * taxRates["higher"]!
         return rounding(tax: higherRateTax + basicBandTax())
     }
     
-    func higherRateTaxDeduction(salary: Double) -> Double {
-        let higherRateTaxable = salary - taxThresholds["basic"]!,
+    func higherRateTaxDeduction(gross: Double) -> Double {
+        let higherRateTaxable = gross - taxThresholds["basic"]!,
             higherRateTax = higherRateTaxable * taxRates["higher"]!
         return rounding(tax: higherRateTax + basicBandTax())
     }
     
-    func basicRateTaxDeduction(salary: Double) -> Double {
-        let basicRateTaxable = salary - taxThresholds["allowance"]!
+    func basicRateTaxDeduction(gross: Double) -> Double {
+        let basicRateTaxable = gross - taxThresholds["allowance"]!
         return rounding(tax: basicRateTaxable * taxRates["basic"]!)
     }
     
@@ -61,9 +61,9 @@ struct TaxCalculator {
         return rounding(tax: basicBand * taxRates["basic"]!)
     }
     
-    func reducePersonalAllowance(salary: Double) -> Double {
+    func reducePersonalAllowance(gross: Double) -> Double {
         var reducedAllowance = taxThresholds["allowance"]
-        for _ in stride(from: taxThresholds["allowanceReduction"]!, to: salary, by: 2) {
+        for _ in stride(from: taxThresholds["allowanceReduction"]!, to: gross, by: 2) {
             if reducedAllowance! >= 1 {
                 reducedAllowance! -= 1
             }
--- SwiftTaxTests/TaxCalculator201920Tests.swift
+++ SwiftTaxTests/TaxCalculator201920Tests.swift
@@ -13,47 +13,67 @@ class TaxCalculator201920Tests: XCTestCase {
     let calc = TaxCalculator()
 
     func testNoTaxDeductedUnderThePersonalAllowance() throws {
-        let net = calc.deductTax(salary: 10000.35)
+        let net = calc.deductTax(gross: 10000.35)
         XCTAssertEqual(net, 10000.35)
     }
     
     func testDeductsBasicTax() throws {
-        let net = calc.deductTax(salary: 20000)
+        let net = calc.deductTax(gross: 20000)
         XCTAssertEqual(net, 18500)
     }
     
     func testDeductsBasicTaxWithDecimal() throws {
-        let net = calc.deductTax(salary: 23000.35)
+        let net = calc.deductTax(gross: 23000.35)
         XCTAssertEqual(net, 20900.28)
     }
     
     func testDeductHigherRateTax() throws {
-        let net = calc.deductTax(salary: 55000)
+        let net = calc.deductTax(gross: 55000)
         XCTAssertEqual(net, 45500)
     }
     
     func testDeductHigherRateTaxWithDecimal() throws {
-        let net = calc.deductTax(salary: 62000.47)
+        let net = calc.deductTax(gross: 62000.47)
         XCTAssertEqual(net, 49700.28)
     }
     
     func testPersonalAllowanceReduction() throws {
-        let net = calc.deductTax(salary: 115000)
+        let net = calc.deductTax(gross: 115000)
         XCTAssertEqual(net, 78500)
     }
     
     func testTotalPersonalAllowanceReduction() throws {
-        let net = calc.deductTax(salary: 135000)
+        let net = calc.deductTax(gross: 135000)
         XCTAssertEqual(net, 88500)
     }
     
     func testAdditionalRateTax() throws {
-        let net = calc.deductTax(salary: 155000)
+        let net = calc.deductTax(gross: 155000)
         XCTAssertEqual(net, 100250)
     }
     
     func testAdditionalRateTaxWithDecimal() throws {
-        let net = calc.deductTax(salary: 200000.75)
+        let net = calc.deductTax(gross: 200000.75)
         XCTAssertEqual(net, 125000.41)
+    }
+    
+    func testPersonaAllowance() throws {
+        let net = calc.deductTax(gross: 12500)
+        XCTAssertEqual(net, 12500)
+    }
+    
+    func testBasicThreshold() throws {
+        let net = calc.deductTax(gross: 50000)
+        XCTAssertEqual(net, 42500)
+    }
+    
+    func testAllowanceReductionLimit() throws {
+        let net = calc.deductTax(gross: 125000)
+        XCTAssertEqual(net, 82500)
+    }
+    
+    func testAdditonalThreshold() throws {
+        let net = calc.deductTax(gross: 150000)
+        XCTAssertEqual(net, 97500)
     }
 }
